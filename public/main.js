44230ab340182c4d9af56fb6d8463bc92a903958
Eliasib García <eliasib.lv.12@gmail.com>
2014-12-26 03:19:00 +0000

    Ahora también se representan gráficamente las notas.

--- public/main.js
+++ public/main.js
@@ -1,7 +1,8 @@
+var result;
 $(document).ready(function() {
   $('#parse').click(function() {
     try {
-      var result = musicL.parse($('#input').val());
+      result = musicL.parse($('#input').val());
       $('#output').html(JSON.stringify(result,undefined,2));
       $('#lienzo').class = "mostrarlienzo";
       var canvas = $('#lienzo')[0];
@@ -74,7 +75,66 @@ $(document).ready(function() {
         }
         stave.addKeySignature(keySignature).setContext(ctx).draw();
         
+        var notas = [];
         
+        for(var i = 0; i < result[0].cuerpo.length; i++) {
+            for (var j = 0; j < result[0].cuerpo[i].notas.length; j++) {
+                var duration = "q";
+                switch(result[0].cuerpo[i].notas[j].figura.valor) {
+                    case "1":
+                        duration = "w";
+                        break;
+                    case "2":
+                        duration = "h";
+                        break;
+                    case "4":
+                        duration = "q";
+                        break;
+                }
+                var n = new Vex.Flow.StaveNote({ keys: [result[0].cuerpo[i].notas[j].nombre.substr(0,1).toLowerCase()+"/4"], duration: duration });
+                if (result[0].cuerpo[i].notas[j].nombre.search('b') != -1)
+                    n.addAccidental(0, new Vex.Flow.Accidental("b"));
+                else if (result[0].cuerpo[i].notas[j].nombre.search('#') != -1)
+                    n.addAccidental(0, new Vex.Flow.Accidental("#"));
+                notas.push(n);
+            }
+            if (i == result[0].cuerpo.length - 1)
+                notas.push(new Vex.Flow.BarNote());
+            notas.push(new Vex.Flow.BarNote());
+        }
+        
+        
+        var voice;
+        switch(result[0].ritmo) {
+            case "2/4":
+                voice = new Vex.Flow.Voice({
+                          num_beats: 2,
+                          beat_value: 4,
+                          resolution: Vex.Flow.RESOLUTION
+                        });
+                break;
+            case "3/4":
+                voice = new Vex.Flow.Voice({
+                          num_beats: 3,
+                          beat_value: 4,
+                          resolution: Vex.Flow.RESOLUTION
+                        });
+                break;
+            case "4/4":
+                voice = new Vex.Flow.Voice({
+                          num_beats: 4,
+                          beat_value: 4,
+                          resolution: Vex.Flow.RESOLUTION
+                        });
+                break;
+        }
+        
+        voice.setStrict(false);
+        voice.addTickables(notas);
+        
+        var formatter = new Vex.Flow.Formatter().joinVoices([voice]).format([voice], 430);
+        
+        voice.draw(ctx,stave);
         
         
     } catch (e) {
